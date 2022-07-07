77ac231f62362322f51add9287adb088dbe317d0
akshay643 <73953645+akshay643@users.noreply.github.com>
2022-07-08 00:56:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+import SearchAppBar from "./components/card";
+
+import Media from "./components/header";
+
+import MediaCard from "./components/bottomCards";
+
+export default function App() {
+  return (
+    <div>
+      {/* <SearchAppBar />
+
+      <Media /> */}
+
+      <MediaCard />
+    </div>
+  );
+}
